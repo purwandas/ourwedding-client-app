05e9c1259d8c12489e329ba7aab18e8202f0cf48
Agesta Saputra <agesta.budy@mekari.com>
2024-07-10 13:26:01 +0700

    chore: init repo

---
+++ pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <h1>Welcome to the Ourwedding Company Profile!</h1>
+  </div>
+</template>
+
+<script setup>
+  onMounted(() => {
+    console.log('Component is mounted')
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
